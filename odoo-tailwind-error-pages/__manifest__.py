1aa8a3528494927cf4a4969014820be7e29e9825
Bashir Hassan <44275879+bshbsh404@users.noreply.github.com>
2020-03-24 11:13:07 +0200

    Update __manifest__.py

--- odoo-tailwind-error-pages/__manifest__.py
+++ odoo-tailwind-error-pages/__manifest__.py
@@ -2,7 +2,6 @@
     'name': 'Tailwind Website Error Pages',
     'category': 'Website',
     'author': 'Root404',
-    'website': 'https://www.app-ness.com',
     'summary': 'Style Odoo website errors based on Tailwind.',
     'depends': ['website', 'base'],
     'images': ['images/Screenshot_1.png','images/Screenshot_2.png','images/Screenshot_3.png',],
