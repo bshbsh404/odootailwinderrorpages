858a5055775564321c0025e6400c83234d683554
bshbsh404 <bshbsh187@gmail.com>
2020-05-13 15:04:17 +0200

    change module name

---
+++ odootailwinderrorpages/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Tailwind Website Error Pages',
+    'category': 'Website',
+    'author': 'Root404',
+    'summary': 'Style Odoo website errors based on Tailwind.',
+    'depends': ['website', 'base'],
+    'images': ['images/Screenshot_1.png','images/Screenshot_2.png','images/Screenshot_3.png',],
+    'data': [
+        'views/views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'AGPL-3',
+}
